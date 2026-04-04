0f557809ed0572d6005ffa4c02794ea72d6dcca6
Criistiiansiito <crigar34@ucm.es>
2026-04-04 15:09:29 +0200

    Nueva screen para modificar recordatorio ya creado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import InicioSesion from './screens/auth/inicioSesion';
 import DashboardPaciente from './screens/pacientes/dashBoardPaciente';
 import DashboardCuidador from './screens/responsables/dashBoardCuidador';
 import Recordatorios from './screens/pacientes/recordatorios';
+import ModificarRecordatorio from './screens/pacientes/modificarRecordatorio';
 import ChatBot from './screens/common/chatBot';
 import Actividades from './screens/pacientes/actividades';
 import ConfiguracionPaciente from './screens/pacientes/configuracionPaciente';
@@ -30,6 +31,7 @@ export default function App() {
         <Stack.Screen name="DashboardCuidador" component={DashboardCuidador} options={{ headerShown: false }} />
         <Stack.Screen name="Configuracion" component={ConfiguracionPaciente} options={{ headerShown: false }} />
         <Stack.Screen name="NuevoRecordatorio" component={NuevoRecordatorio} options={{ headerShown: false }} />
+        <Stack.Screen name="ModificarRecordatorio" component={ModificarRecordatorio} options={{ headerShown: false }} />
         <Stack.Screen name="Calendario" component={Calendario} options={{ headerShown: false }} />
         <Stack.Screen name="Recordatorios" component={Recordatorios} options={{ headerShown: false }} />
         <Stack.Screen name="ChatBot" component={ChatBot} options={{ headerShown: false }} />
